b43230bae0e8250628944fda24411e018db96b73
flytreeleft <flytreeleft@126.com>
2017-03-14 18:41:23 +0800

    immutable: export some utilities

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 import Immutable from './Immutable';
 
+export {default as getNodeByPath} from './node/getNodeByPath';
+export {default as createNE} from './utils/createNE';
+export {default as valueOf} from './utils/valueOf';
+export {default as isArray} from './utils/isArray';
+export {default as isObject} from './utils/isObject';
+export {default as isPlainObject} from './utils/isPlainObject';
+export {default as isFunction} from './utils/isFunction';
+export {default as isPrimitive} from './utils/isPrimitive';
+export {default as isDate} from './utils/isDate';
+export {default as hasOwn} from './utils/hasOwn';
+export {default as isConfigurable} from './utils/isConfigurable';
+export {default as isWritable} from './utils/isWritable';
+
 export default Immutable;
